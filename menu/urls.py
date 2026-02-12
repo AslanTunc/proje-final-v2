8f8914d234222270b573b321800ede28b6029954
AslanTunc <aslantunc2001@gmail.com>
2026-02-12 18:33:36 +0100

    QR kodu sayfasi ve yolu eklendi

--- menu/urls.py
+++ menu/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.menu_list, name='menu_list'),
+    path('qr/', views.qr_code_view, name='qr_code'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -8,3 +8,7 @@ def menu_list(request):
         'categories': categories,
     }
     return render(request, 'menu/index.html', context)
+
+def qr_code_view(request):
+    # Bu fonksiyon sadece QR kodun olduğu sayfayı açar
+    return render(request, 'menu/qr_code.html')
