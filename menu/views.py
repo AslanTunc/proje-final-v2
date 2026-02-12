60de68aa7a6809d7ceb5e218f13b5e3e72c9c116
AslanTunc <aslantunc2001@gmail.com>
2026-02-12 18:24:31 +0100

    Prefetch related name items olarak duzeltildi

--- menu/views.py
+++ menu/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Category, MenuItem
 
 def menu_list(request):
-    categories = Category.objects.prefetch_related('menuitem_set').all()
+    categories = Category.objects.prefetch_related('items').all()
     
     context = {
         'categories': categories,
